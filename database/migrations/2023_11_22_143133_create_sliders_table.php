a7cc894e7624dab55f05d23a98089a2adcdecf15
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2023-11-22 15:38:31 +0100

    update default stubs ,  add new Models to the app

---
+++ database/migrations/2023_11_22_143133_create_sliders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->nullable();
+            $table->string('title',200);
+            $table->mediumText('content')->nullable();
+            $table->string('image')->nullable();
+
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sliders');
+    }
+};
